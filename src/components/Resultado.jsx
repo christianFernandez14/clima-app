29231341f8390dece1bbae9d4c143f76a7dee798
Christian Fernandez <christian14@gmail.com>
2023-12-04 21:41:08 -0300

    Accediendo y mostrando el Resultado de la consulta II Parte

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -4,13 +4,28 @@ const Resultado = () => {
 
   const { resultado } = useClima()
 
-  const { name } = resultado
+  const { name, main } = resultado
+
+  // La data que viene de la Api esta el Kelvin
+  const kelvin = 273.15
 
   console.log(resultado)
 
   return (
-    <div className="contenedor">
-      <h2>El clima de { name } es:</h2>
+    <div className="contenedor clima">
+      <h2>El clima de {(name)} es:</h2>
+      <p>
+        {parseInt(main.temp - kelvin)} <span>&#x2103;</span>
+      </p>
+      <div className="temp_min_max">
+        <p>
+          Min: {parseInt(main.temp_min - kelvin)} <span>&#x2103;</span>
+        </p>
+        <p>
+          Max: {parseInt(main.temp_max - kelvin)} <span>&#x2103;</span>
+        </p>
+      </div>
+
     </div>
   )
 }
