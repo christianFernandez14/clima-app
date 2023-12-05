e6aac9ab858124214d6413fb6f6bb11a88ccff3a
Christian Fernandez <christian14@gmail.com>
2023-12-04 21:19:36 -0300

    Accediendo y mostrando el Resultado de la consulta

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,18 @@
+import useClima from "../hooks/useClima"
+
+const Resultado = () => {
+
+  const { resultado } = useClima()
+
+  const { name } = resultado
+
+  console.log(resultado)
+
+  return (
+    <div className="contenedor">
+      <h2>El clima de { name } es:</h2>
+    </div>
+  )
+}
+
+export default Resultado
